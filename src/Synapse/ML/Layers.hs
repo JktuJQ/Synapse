eb188c5fafe6c5efa206beca2dbfb211f3ac1066
JktuJQ <odmamontov@gmail.com>
2025-02-17 01:11:15 +0300

    [Development] Re-exported `Synapse.ML.Layers` submodules`
    Re-exported `Synapse.ML.Layers` submodules` in 'Synapse.ML.Layers.hs'.

--- src/Synapse/ML/Layers.hs
+++ src/Synapse/ML/Layers.hs
@@ -3,5 +3,12 @@
 
 
 module Synapse.ML.Layers
-    (
+    ( -- * Re-exports
+
+      module Synapse.ML.Layers.Layer
+    , module Synapse.ML.Layers.Activations
     ) where
+
+
+import Synapse.ML.Layers.Layer
+import Synapse.ML.Layers.Activations
