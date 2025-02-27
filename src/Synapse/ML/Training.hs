cf7ed3837c918b7024d2d7e5b05abd6e1e415345
JktuJQ <odmamontov@gmail.com>
2025-02-27 22:38:54 +0300

    [Development] Re-exported `Synapse.ML.Training` submodules
    Re-exported `Synapse.ML.Training` submodules.

--- src/Synapse/ML/Training.hs
+++ src/Synapse/ML/Training.hs
@@ -3,5 +3,16 @@
 
 
 module Synapse.ML.Training
-    (
+    ( -- * Re-exports
+      module Synapse.ML.Training.Losses
+    , module Synapse.ML.Training.Metrics
+
+    , module Synapse.ML.Training.LearningRates
+    , module Synapse.ML.Training.Batching
     ) where
+
+import Synapse.ML.Training.Losses
+import Synapse.ML.Training.Metrics
+
+import Synapse.ML.Training.LearningRates
+import Synapse.ML.Training.Batching
